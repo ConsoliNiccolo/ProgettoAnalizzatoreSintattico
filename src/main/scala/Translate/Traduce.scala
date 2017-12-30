bdf94d0e0416cbb41f3e66919541675d2988bc7b
ConsoliNiccolo <32384851+ConsoliNiccolo@users.noreply.github.com>
2017-12-30 16:09:20 +0100

    Aggiornato il traduttore "Traduce.scala"

--- src/main/scala/Translate/Traduce.scala
+++ src/main/scala/Translate/Traduce.scala
@@ -4,19 +4,26 @@ import java.util.regex.Pattern
 import scala.collection.mutable.ListBuffer
 
 class Traduce extends App {
+  var check:Boolean=false;
 
   def Trasl(input: String): String = {
     println(input)
     var checking_ok=input.contains("parsed")
     checking_ok match{
       case true => {
-        println("Parsed done successfully with trasl2!")
+        println("Parsed eseguito correttemente,operazione di traduzione in corso:")
         var str=input.slice(15,input.length)
         val res:String=startToConvert(str)
         println(res)
         return res
       }
-      case false => return "Parsed did not go well! Check it next time!"
+      case _ => {
+        var error_split: Array[String]=input.split("failure:")
+        var error2_split: Array[String]=error_split(1).split("found")
+        return "-------------------------------------------------------"+"\n"+
+               "Errore in posizione: "+error_split(0)+"\nTipologia errore: "+error2_split(0)+
+               " found"+"\n-------------------------------------------------------"
+      }
     }
 
   }
@@ -25,60 +32,76 @@ class Traduce extends App {
     var some_split: Array[String]=input.split("Some")
     var risultato: String="!"
     for (x <- some_split) {
-      if(x.contains("i, n, t")) {risultato=risultato+"$"+">"+"int"+"<";
-        val ex:Array[String]=x.split("\\(i, n, t, ")
-        val ind=ex(1).indexOf(')')
-        risultato=risultato+">"+onLookingForID(ind,ex(1))+"<>;<<"
-        val stri:Array[String]=ex(1).split("~=\\)~\\(")
-        val ind2=stri(1).indexOf(')')
-        ind2 match {
-          case 7 => risultato=risultato+"&>>"+onLookingForID(ind,ex(1))+"<>=<>"+stri(1).charAt(0)+"<<"
-          case 8 => risultato=risultato+"&>>"+onLookingForID(ind,ex(1))+"<>=<>"+stri(1).charAt(0)+stri(1).charAt(ind2-1)+"<<"
-          case x if(ind2>8) =>
-          {risultato=risultato+"&>>"+onLookingForID(ind,ex(1))+"<>=<>"+onLookingForValue(ind2,stri(1))}
-        }
+        if(x.contains("int")) {check=true}//Dichiarazione}
+        else if(!x.contains("int")) {check=false}//Assegnazione}
+            check match {
+                  case true => {
+                      val strings:Array[String]=x.split("~List")
+                      for(x <-strings) {
+                      if(x.contains("))~;)")) {
+                        //Vera stringa di dichiarazione
+                        val ind=x.indexOf(')')
+                        risultato=risultato+"$"+">"+"int"+"<"+">"+onLookingForID(ind,x,check)+"<>;<<"
+                      }
+                    }
+                  }
+                  //Assegnazione
+                  case false => {
+                        val ind=x.indexOf(')')
+                        //Identificatore Memorizzato.
+                        risultato=risultato+"&"+">"+onLookingForID(ind,x,check)+"<>="
+                        //Ricerca valori
+                        val stri:Array[String]=x.split("~=\\)~\\(")
+                          if(stri(1).contains("+") || stri(1).contains("*")){
+                              val st:Array[String]=stri(1).split("List\\(\\)")
+                              for(x<-st) {
+                                //Matcha sia numeri che 1ID che multiID
+                                val mypat=raw"([0-9]+)|(([a-z][,][ ]){1,}[a-zA-Z][)])|([(][a-zA-Z][)])|([+])|([*])".r
+                                val all=mypat.findAllIn(x)
+                                for(x <- all) {
+                                  //Pulizia e risultato=risultato..
+                                    val ns=x.replace(",","")
+                                    val ns2=ns
+                                    val ns3=ns2.replace(" ","")
+                                    val ns4=ns3.replace(")","")
+                                    val ns5=ns4.replace("(","")
+                                    risultato=risultato+"<>"+ns5}
+                              }
+                            risultato=risultato+"<<"
+                          }
+                  }
+            }
 
-      }
-      if(x.contains("None~;")) {risultato=risultato+"&>;<<"}
+        if(x.contains("None~;")) {risultato=risultato+"&>;<<"}
     }
 
     return risultato
   }
 
-  def onLookingForID(index: Int,str: String): String = {
+
+
+  def onLookingForID(index: Int,str: String,b: Boolean): String = {
     var res: String=""
-    index match {
-      case 1 => res=""+str.charAt(0)
-      case x if index > 1 => {
-        var contatore=(index-1)/3
-        res=res+str.charAt(0)+onLookingForID2(index,str,contatore)
-      }
-      case _ => return("No ID?")
+    var minimum_count: Int=0;
+    if(b) {minimum_count=1}
+    else {minimum_count=8}
+    while(minimum_count<=index) {
+      res=res+str.charAt(minimum_count)
+      minimum_count=minimum_count+3;
     }
-    return res
+    return res;
 
-  }
-
-  def onLookingForID2(index: Int,str: String,count: Int): String ={
-    var res:String=""
-    var conto: Int=count
-    var help: Int=3
-    while(conto>0){
-      res=res+str.charAt(help)
-      help=help+3
-      conto=conto-1
-    }
-    return res
   }
 
   def onLookingForValue(index: Int,str:String): String ={
-    var res: String=""+str.charAt(0)+str.charAt(7)
-    var count: Int=index;
-    while(count>8) {
-      res=res+str.charAt(count-1)
-      count=count-3
+    var res: String=""
+    var count: Int=1;
+    while(count!=index) {
+      res=res+str.charAt(count)
+      count=count+1
     }
     return res;
   }
 
 }
+
